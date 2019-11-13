832f76764ee10587aa6e2a251621a3c51a1861a7
Marcelo Almeida <mpaemail@gmail.com>
2019-11-13 15:40:08 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/com/aprendizadomarcelo/demo/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendizadomarcelo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
